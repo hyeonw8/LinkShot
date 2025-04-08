2fe59a39ba901bf7204576353829de1a3070df75
hyeonw8 <whgusrud223@gmail.com>
2025-04-08 22:44:51 +0900

    💄 style: SkeletonListSection 카드 레이아웃 개선 및 스켈레톤 구성 요소 정교화

--- src/components/skeleton/SkeletonListSection.tsx
+++ src/components/skeleton/SkeletonListSection.tsx
@@ -3,21 +3,48 @@ interface SkeletonListSectionProps {
   count?: number;
 }
 
-export const SkeletonListSection = ({ title, count = 6 }: SkeletonListSectionProps) => {
+export const SkeletonListSection = ({
+  title,
+  count = 8,
+}: SkeletonListSectionProps) => {
   return (
     <div className="w-full">
       <h2 className="text-lg font-bold mb-3 pl-1">{title}</h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
         {Array.from({ length: count }).map((_, idx) => (
-          <div
-            key={idx}
-            className="border p-4 rounded-md shadow space-y-2 animate-pulse h-[300px]"
-          >
-            <div className="h-4 w-24 bg-gray-300 rounded" />
-            <div className="w-full h-32 bg-gray-300 rounded-md" />
-            <div className="h-6 w-3/4 bg-gray-300 rounded" />
-            <div className="h-4 w-full bg-gray-300 rounded" />
-            <div className="h-4 w-5/6 bg-gray-300 rounded" />
+          <div className="border border-gray-200  rounded-lg animate-pulse shadow-sm hover:shadow-md transition-shadow duration-300 h-full flex flex-col">
+            {/* 이미지 섹션 */}
+            <div className="h-32 bg-gray-100 "></div>
+            {/* 콘텐츠 섹션 */}
+            <div className="p-4 flex-grow flex flex-col gap-y-3">
+              <div className="flex flex-col gap-y-2">
+                {/* 날짜 */}
+                <div className="h-3 w-4/6 bg-gray-300 rounded"></div>
+                {/* 제목 */}
+                <div className="h-5 w-full bg-gray-300 rounded"></div>
+                <div className="h-5 w-full bg-gray-300 rounded"></div>
+                {/* 설명 */}
+                <div className="h-3 w-5/6 bg-gray-300 rounded"></div>
+                <div className="h-3 w-5/6 bg-gray-300 rounded"></div>
+              </div>
+              {/* 버튼 그룹 */}
+              <div className="flex flex-col gap-y-2 ">
+                <div className="flex flex-row gap-2 justify-end">
+                  {/* 고정 버튼 */}
+                  <div className="h-9 w-2/6 bg-gray-300 rounded-md" />
+                  {/* 수정 버튼 */}
+                  <div className="h-9 w-1/6 bg-gray-300 rounded-md" />
+                  {/* 삭제 버튼 */}
+                  <div className="h-9 w-1/6 bg-gray-300 rounded-md" />
+                </div>
+                <div className="flex flex-row gap-2 justify-end">
+                  {/* 바로가기 버튼*/}
+                  <div className="h-9 w-2/6 bg-gray-300 rounded-md" />
+                  {/* 공유하기 버튼 */}
+                  <div className="h-9 w-2/6 bg-gray-300 rounded-md" />
+                </div>
+              </div>
+            </div>
           </div>
         ))}
       </div>
